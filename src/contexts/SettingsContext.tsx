adcbdaf244f51bdedbedbf586ac8bf30347c3aca
oleksandr-jr <oleksandr.cho@gmail.com>
2025-11-27 15:11:24 +0100

    Implemented a datatable in the event manager

--- src/contexts/SettingsContext.tsx
+++ src/contexts/SettingsContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState, useEffect, useMemo, ReactNode } from 'react'
+import { createContext, useContext, useState, useEffect, useMemo, ReactNode } from 'react'
 import { api } from '@/lib/api'
 
 interface Settings {
